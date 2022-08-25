3a938ddfe9d53b3b6faa7d10909f2e3a9dd51155
waghmodeomkar0809 <waghmodeomkar975@gmail.com>
2022-08-25 18:19:59 +0530

    add DieRoll

--- SnakeAndLadder/Game.cs
+++ SnakeAndLadder/Game.cs
@@ -9,5 +9,11 @@ namespace SnakeAndLadder
     public class Game
     {
         int playerPosition = 0;
+        public int DieRoll()
+        {
+            Random random = new Random();
+            int dieCount=random.Next(1,7);
+            return dieCount;
+        }
     }
 }
