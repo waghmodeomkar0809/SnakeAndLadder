ae8c744a682e482ba8ecb45cb74f1f580c688d72
waghmodeomkar0809 <waghmodeomkar975@gmail.com>
2022-08-25 18:50:07 +0530

    add PlayerCheck

--- SnakeAndLadder/Game.cs
+++ SnakeAndLadder/Game.cs
@@ -9,11 +9,27 @@ namespace SnakeAndLadder
     public class Game
     {
         int playerPosition = 0;
+        const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        Random random = new Random();
         public int DieRoll()
         {
-            Random random = new Random();
+           
             int dieCount=random.Next(1,7);
             return dieCount;
+        }
+        public void Play()
+        {
+            int option = random.Next(0,3);
+            switch (option)
+            {
+                case NO_PLAY: playerPosition += 0;
+                    break;
+                case LADDER: playerPosition += DieRoll();
+                    break;
+                case SNAKE: playerPosition -= DieRoll();
+                    break;
+            }
+            Console.WriteLine(playerPosition);
         }
     }
 }
